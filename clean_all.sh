48c7f0c1b23fc33d79b36313b23a1f4e90c818ce
ochavarr <ochavarr@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2007-06-26 02:07:09 +0000

    Updated erasing of temporal data and directories.
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@138 9e65a30e-a369-483d-98ba-aeb80c2fce51

--- clean_all.sh
+++ clean_all.sh
@@ -1,6 +1,6 @@
 cd pkgs/SpharmonicKit27;make clean;cd ../..
-rm -rf `find . -name "*~"` `find . -name "*.jar"` `find . -name "*.class"` `find . -name "output.jpg"` ./doc/html_doxygen ./doc/html_javadoc
-rm -rf `find . -type d -name "lib"` `find . -type d -name "classes"`
+rm -rf `find . -name "*~"` `find . -name "*.jar"` `find . -name "*.war"` `find . -name "*.class"` `find . -name "output.jpg"` ./doc/html_doxygen ./doc/html_javadoc
+rm -rf `find . -type d -name "lib"` `find . -type d -name "classes"` `find . -type d -name "tmp"`
 if [ -f ./testsuite/ApplicationCases/SearchEngineFor3DModels/etc/metadata.bin ]; then
     rm -i ./testsuite/ApplicationCases/SearchEngineFor3DModels/etc/metadata.bin
 fi
