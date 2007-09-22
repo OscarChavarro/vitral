5bffc95e06d456632a938145364ac2356b8d5dcd
ochavarr <ochavarr@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2007-09-22 16:36:08 +0000

    Javadoc configuration updated
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@219 9e65a30e-a369-483d-98ba-aeb80c2fce51

--- document.sh
+++ document.sh
@@ -4,4 +4,4 @@ echo "This could take a time of around 11 minutes, please wait..."
 rm -rf doc/html_doxygen
 rm -rf doc/html_javadoc
 time doxygen doc/_doxygen/doxyfile.config
-javadoc -d ./doc/html_javadoc src/vsdk/toolkit/common/*.java src/vsdk/toolkit/environment/*.java src/vsdk/toolkit/environment/geometry/*.java src/vsdk/toolkit/gui/*.java src/vsdk/toolkit/media/*.java src/vsdk/toolkit/io/*.java src/vsdk/toolkit/io/geometry/*.java src/vsdk/toolkit/io/image/*.java src/vsdk/toolkit/render/*.java src/vsdk/toolkit/render/jogl/*.java src/vsdk/toolkit/render/awt/*.java
+javadoc -d ./doc/html_javadoc ./src/vsdk/framework/* ./src/vsdk/framework/shapeMatching/* ./src/vsdk/toolkit/common/* ./src/vsdk/toolkit/environment/* ./src/vsdk/toolkit/environment/geometry/* ./src/vsdk/toolkit/environment/geometry/polyhedralBoundedSolidNodes/* ./src/vsdk/toolkit/environment/scene/* ./src/vsdk/toolkit/gui/* ./src/vsdk/toolkit/io/* ./src/vsdk/toolkit/io/geometry/* ./src/vsdk/toolkit/io/image/* ./src/vsdk/toolkit/io/metadata/* ./src/vsdk/toolkit/media/* ./src/vsdk/toolkit/processing/* ./src/vsdk/toolkit/render/* ./src/vsdk/toolkit/render/awt/* ./src/vsdk/toolkit/render/jogl/*
