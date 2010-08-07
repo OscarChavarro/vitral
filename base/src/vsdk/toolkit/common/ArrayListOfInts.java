49676ab8dcd35236534eb4cf2fcfece5806a1673
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2010-08-07 03:05:35 +0000

    Addad quicksort algorithm.
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@487 9e65a30e-a369-483d-98ba-aeb80c2fce51

--- base/src/vsdk/toolkit/common/ArrayListOfInts.java
+++ base/src/vsdk/toolkit/common/ArrayListOfInts.java
@@ -65,6 +65,42 @@ public class ArrayListOfInts extends FundamentalEntity {
     {
         mysize = 0;
     }
+
+    /**
+    Quicksort
+    */
+    public static void quicksort(int[] array, int left0, int right0)
+    {
+      int left = left0;
+      int right = right0 + 1;
+      int pivot, temp;
+
+      pivot = array[left0];
+      do {
+        do left++; while (left <= right0 && array[left] < pivot);
+        do right--; while (array[right] > pivot );
+        if (left < right) {
+          temp = array[left];
+          array[left] = array[right];
+          array[right] = temp;
+        }
+      } while (left <= right);
+
+      temp = array[left0];
+      array[left0] = array[right];
+      array[right] = temp;
+
+      if (left0 < right) quicksort(array, left0, right);
+      if (left < right0) quicksort(array, left, right0);
+    }
+
+    /**
+    Quicksort
+    */
+    public void sort() {
+        quicksort(array, 0, mysize-1);
+    }
+
 }
 
 //===========================================================================
