d32a6b37b596cc231877e99d323591ecff7e6c2c
taake <taake@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2012-10-09 23:20:13 +0000

    Small design changes.
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@564 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ testsuite/ApplicationCases/SceneEditorApplication/src_lib/vsdk/toolkit/gui/CommandListener.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vsdk.toolkit.gui;
+
+/**
+ *
+ * @author TaakeSlottet
+ */
+public interface CommandListener {
+    public boolean executeCommand(String commandId);
+}
