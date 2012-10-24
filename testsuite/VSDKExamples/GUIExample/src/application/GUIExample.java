ebb3b3ed968aff822948befcf61a67bbad97e4f8
taake <taake@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2012-10-24 00:37:28 +0000

    Modified test gui example with the GUI file
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@577 9e65a30e-a369-483d-98ba-aeb80c2fce51

--- testsuite/VSDKExamples/GUIExample/src/application/GUIExample.java
+++ testsuite/VSDKExamples/GUIExample/src/application/GUIExample.java
@@ -5,16 +5,20 @@
 package application;
 
 // Internal classes
+import com.sun.java.swing.plaf.gtk.GTKConstants;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.TextArea;
 import java.io.FileInputStream;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.border.TitledBorder;
 import vsdk.toolkit.gui.Gui;
+import vsdk.toolkit.gui.GuiDialog;
+import vsdk.toolkit.gui.variable.GuiVariable;
 import vsdk.toolkit.io.gui.GuiPersistence;
 import vsdk.toolkit.render.swing.SwingGuiRenderer;
 
@@ -25,7 +29,7 @@ public class GUIExample {
     private String languageGuiFile;
     private JFrame mainWindowWidget;
     private GUIEventExecutor executor;
-    
+
     /**
      * @param args the command line arguments
      */
@@ -36,35 +40,64 @@ public class GUIExample {
 
     public GUIExample() {
         executor = new GUIEventExecutor();
-    }    
-    
-    public void createGUI(){
+    }
+
+    public void createGUI() {
         languageGuiFile = "./etc/english.gui";
-        
+
         mainWindowWidget = new JFrame("VITRAL GUI Example");
         mainWindowWidget.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       
+
         try {
             gui = GuiPersistence.importAquynzaGui(
-                                new FileInputStream(languageGuiFile));
+                    new FileInputStream(languageGuiFile));
             System.out.println(gui);
-        }
-        catch ( Exception e ) {
+        } catch (Exception e) {
             System.err.println("Fatal error: can not open GUI file");
+            e.printStackTrace();
             System.exit(0);
         }
         JMenuBar menubar;
         menubar = SwingGuiRenderer.buildMenubar(gui, null, executor);
-        
-        Dimension d = new Dimension(800, 600);
+
+        Dimension d = new Dimension(1200, 600);
 
         JPanel p = new JPanel();
-        p.setBackground(Color.red);
-        mainWindowWidget.getContentPane().add(p,BorderLayout.CENTER);
-        
-        JPanel q = SwingGuiRenderer.buildVariable(gui.getVariableByName("color"));
-        p.add(q);
-        
+        p.setBackground(Color.WHITE);
+
+
+        /*
+         * Dialog test
+         */
+        for (int i = 0; i < gui.getDialogList().size(); i++) {
+            GuiDialog dial = gui.getDialogList().get(i);
+            JPanel panel = SwingGuiRenderer.buildDialog(gui.getDialogList().get(i));
+
+            for (int j = 0; j < dial.getChildren().size(); j++) {
+                GuiDialog dialChild = (GuiDialog) dial.getChildren().get(j);
+                JPanel panelChild = SwingGuiRenderer.buildDialog(dialChild);
+  
+                for (int k = 0; k < dialChild.getPendingVariableNames().size(); k++) {
+                    JPanel q = SwingGuiRenderer.buildVariable(gui.getVariableByName(dialChild.getPendingVariableNames().get(k)));
+                    panelChild.add(q);
+                }
+                panel.add(panelChild);
+            }
+            
+            for (int k = 0; k < dial.getPendingVariableNames().size(); k++) {
+                JPanel q = SwingGuiRenderer.buildVariable(gui.getVariableByName(dial.getPendingVariableNames().get(k)));
+                panel.add(q);
+            }
+            p.add(panel);
+        }
+
+        TitledBorder tb = new TitledBorder("Test Sphere Modifier");
+        p.setBorder(tb);
+        p.setBackground(Color.DARK_GRAY);
+        mainWindowWidget.getContentPane().add(p);
+
+        //mainWindowWidget.add(panel,BorderLayout.SOUTH);
+
         mainWindowWidget.setJMenuBar(menubar);
         mainWindowWidget.setPreferredSize(d);
         mainWindowWidget.pack();
