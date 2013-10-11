fe36f49671d3f2246638c23ce1bd8fafe39c3e16
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2013-10-11 15:41:44 +0000

    Update...
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@593 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ testsuite/VSDKExamples/MeshExample/src/Animador.java
@@ -0,0 +1,28 @@
+public class Animador implements Runnable
+{
+    public double t;
+    public MeshExample bicho;
+
+    public Animador(MeshExample padre)
+    {
+	bicho = padre;
+    }
+
+    public void run() {
+        t = 0.0;
+        try {
+            while ( true ) {
+                t = t + 0.1;
+                System.out.println("T = 1" + t);
+                bicho.x = t;
+                bicho.canvas.repaint();
+                Thread.sleep(1000/24);
+                if ( t > 5.0zx ) {
+                    break;
+		}
+	    }
+	}
+	catch ( Exception e ) {
+	}
+    }
+}
