8cc9e62f7a11fba3666f11e0a9cfefc257c6e0ce
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2013-10-19 00:21:49 +0000

    - 34min: checking how to add touch events on android: first version of     CameraControllerAquynza working on android smartphone :)   - 53min: Checking how to use GPS on Android. Done simple gps events :)
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@611 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ android/src/vsdk/toolkit/gui/AndroidSystem.java
@@ -0,0 +1,26 @@
+//===========================================================================
+
+package vsdk.toolkit.gui;
+
+// Android packages
+import android.view.MotionEvent;
+
+public class AndroidSystem
+{
+    public static MouseEvent android2vsdkEvent(MotionEvent eandroid)
+    {
+        MouseEvent evsdk;
+
+        evsdk = new MouseEvent();
+        evsdk.setX((int)Math.floor(eandroid.getX()));
+        evsdk.setY((int)Math.floor(eandroid.getY()));
+        evsdk.setButton(MouseEvent.BUTTON1);
+        evsdk.setModifiers(MouseEvent.BUTTON1_DOWN_MASK);
+
+        return evsdk;
+    }
+}
+
+//===========================================================================
+//= EOF                                                                     =
+//===========================================================================
