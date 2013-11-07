06b8f5841111700dd9682525826f7f57413932ba
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2013-11-07 16:29:50 +0000

    Idem.
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@635 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ android/src/vsdk/toolkit/render/androidgles20/AndroidGLES20MaterialRenderer.java
@@ -0,0 +1,19 @@
+//===========================================================================
+
+package vsdk.toolkit.render.androidgles20;
+
+// VSDK classes
+import vsdk.toolkit.environment.Material;
+
+public class AndroidGLES20MaterialRenderer extends AndroidGLES20Renderer
+{
+    public static void activate(Material m)
+    {
+        currentMaterial = new Material(m);
+        activateShaders();
+    }
+}
+
+//===========================================================================
+//= EOF                                                                     =
+//===========================================================================
