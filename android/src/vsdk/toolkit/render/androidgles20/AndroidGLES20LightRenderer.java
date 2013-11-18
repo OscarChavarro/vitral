63483183d8fb79cc53ff1e04fd66e68d9b5288d0
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2013-11-18 00:41:52 +0000

    - 1h45min: AndroidGLES20LightRenderer   - 1h46min: checking gouraud vertex shader for light position value and     specular factor   - 27min: checking gouraud vertex shader for direction to the viewer   - 30min: checking gouraud vertex shader for multiple lights   - 1h: gouraud shader working with multiple lights :)
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@638 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ android/src/vsdk/toolkit/render/androidgles20/AndroidGLES20LightRenderer.java
@@ -0,0 +1,123 @@
+//===========================================================================
+
+package vsdk.toolkit.render.androidgles20;
+
+// Java basic classes
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
+// Android classes
+import android.opengl.GLES20;
+
+// VSDK classes
+import vsdk.toolkit.common.ColorRgb;
+import vsdk.toolkit.common.RendererConfiguration;
+import vsdk.toolkit.common.linealAlgebra.Vector3D;
+import vsdk.toolkit.environment.Light;
+
+public class AndroidGLES20LightRenderer extends AndroidGLES20Renderer
+{
+    public static void activate(Light l)
+    {
+        int i;
+
+	for ( i = 0; i < lights.size(); i++ ) {
+	    if ( lights.get(i) == l ) {
+                return;
+	    }
+	}
+        lights.add(l);
+    }
+
+    public static void draw(Light l)
+    {
+        glMatrixMode(GL_MODELVIEW);
+        glPushMatrix();
+        glLoadIdentity();
+
+        glDisable(GL_TEXTURE_2D);
+        setShadingType(RendererConfiguration.SHADING_TYPE_NOLIGHT);
+        activateShaders();
+
+        //-----------------------------------------------------------------
+        int vertexSizeInBytes = FLOAT_SIZE_IN_BYTES * 8;
+        int index = 0;
+        ColorRgb c = l.getSpecular();
+        float delta = 0.1f;
+        int numVertex = 6;
+
+        float vertexDataArray[] = new float[numVertex*8];
+
+        Vector3D p = l.getPosition();
+
+        vertexDataArray[index] = (float)p.x - delta;    index++;
+        vertexDataArray[index] = (float)p.y;    index++;
+        vertexDataArray[index] = (float)p.z;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        vertexDataArray[index] = (float)p.x + delta;    index++;
+        vertexDataArray[index] = (float)p.y;    index++;
+        vertexDataArray[index] = (float)p.z;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        vertexDataArray[index] = (float)p.x;    index++;
+        vertexDataArray[index] = (float)p.y - delta;    index++;
+        vertexDataArray[index] = (float)p.z;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        vertexDataArray[index] = (float)p.x;    index++;
+        vertexDataArray[index] = (float)p.y + delta;    index++;
+        vertexDataArray[index] = (float)p.z;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        vertexDataArray[index] = (float)p.x;    index++;
+        vertexDataArray[index] = (float)p.y;    index++;
+        vertexDataArray[index] = (float)p.z - delta;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        vertexDataArray[index] = (float)p.x;    index++;
+        vertexDataArray[index] = (float)p.y;    index++;
+        vertexDataArray[index] = (float)p.z + delta;    index++;
+        vertexDataArray[index] = (float)c.r;    index++;
+        vertexDataArray[index] = (float)c.g;    index++;
+        vertexDataArray[index] = (float)c.b;    index++;
+        vertexDataArray[index] = 0.0f;          index++; // u
+        vertexDataArray[index] = 0.0f;          index++; // v
+
+        //------------------------------------------------------------
+        FloatBuffer verticesBufferedArray;
+
+        verticesBufferedArray = ByteBuffer.allocateDirect(
+            vertexDataArray.length * FLOAT_SIZE_IN_BYTES).order(
+            ByteOrder.nativeOrder()).asFloatBuffer();
+        verticesBufferedArray.put(vertexDataArray);
+        drawVertices3Position3Color2Uv(verticesBufferedArray, 
+            GLES20.GL_LINES, numVertex, vertexSizeInBytes);
+
+    }
+}
+
+//===========================================================================
+//= EOF                                                                     =
+//===========================================================================
