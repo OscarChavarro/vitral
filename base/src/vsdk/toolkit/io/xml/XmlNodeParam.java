b65ebf005b4ad61ae33083f75b112312fc049a7e
jedilink <jedilink@9e65a30e-a369-483d-98ba-aeb80c2fce51>
2014-05-22 00:39:52 +0000

    XML custom library functions to import big XML files.
    git-svn-id: https://svn.code.sf.net/p/vitral/code/vitral/trunk@746 9e65a30e-a369-483d-98ba-aeb80c2fce51

---
+++ base/src/vsdk/toolkit/io/xml/XmlNodeParam.java
@@ -0,0 +1,18 @@
+//===========================================================================
+package vsdk.toolkit.io.xml;
+
+public class XmlNodeParam
+{
+    public String param;
+    public String value;
+
+    public XmlNodeParam(String param)
+    {
+        this.param = new String(param);
+        value = null;
+    }
+}
+
+//===========================================================================
+//= EOF                                                                     =
+//===========================================================================
